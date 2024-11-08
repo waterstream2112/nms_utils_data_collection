3aa5624bc9c523b27223a9648da853ebcda42445
Ho Hoan Nghia <waterstream2112@gmail.com>
2024-11-09 07:53:11 +0800

    update debug msg

---
+++ launch/node_lidar_imu_time_offset.launch.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+    
+    # Declare remapping
+    remappings = [("/pointcloud", "/livox/lidar"), 
+                  ("/imu", "/livox/imu")]
+    
+    # Declare node actions
+    node_lidar_imu_time_offset = Node(
+        package="nms_utils_data_collection",
+        name="node_lidar_imu_time_offset",
+        executable="node_lidar_imu_time_offset",
+        remappings=remappings,
+        output="screen"
+    )
+    
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Add node actions to the launch description
+    ld.add_action(node_lidar_imu_time_offset)
+    
+    return ld
--- src/node_lidar_imu_time_offset.cpp
+++ src/node_lidar_imu_time_offset.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp/time.hpp"
+#include <sensor_msgs/msg/imu.hpp>
 #include "sensor_msgs/msg/point_cloud2.hpp"
 
 #include <pcl/common/impl/transforms.hpp>
@@ -13,14 +15,65 @@
 class NodeLidarImuTimeOffset : public rclcpp::Node
 {
 private:
+    rclcpp::Time imuTimeStamp = this->now();
+    rclcpp::Time pointcloudTimeStamp = this->now();
 
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imuSub_;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloudSub_;
+
 
 public:
     NodeLidarImuTimeOffset() : rclcpp::Node("node_lidar_imu_time_offset")
     {
+
+        // Subscription
+        imuSub_ = this->create_subscription<sensor_msgs::msg::Imu>(
+                                "/imu", 
+                                10, 
+                                std::bind(&NodeLidarImuTimeOffset::imuCallback, this, std::placeholders::_1));
+        pointcloudSub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+                                "/pointcloud", 
+                                10, 
+                                std::bind(&NodeLidarImuTimeOffset::pointcloudCallback, this, std::placeholders::_1));
+    }
+
+
+    void imuCallback(const sensor_msgs::msg::Imu::ConstSharedPtr &msg) {
+        // RCLCPP_INFO(this->get_logger(), "Orientation: [%f, %f, %f]",
+        //             msg->orientation.x, msg->orientation.y, msg->orientation.z);
+        // RCLCPP_INFO(this->get_logger(), "Angular Velocity: [%f, %f, %f]",
+        //             msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z);
+        // RCLCPP_INFO(this->get_logger(), "Linear Acceleration: [%f, %f, %f]",
+        //             msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z);
+
+        rclcpp::Duration elapse = this->now() - imuTimeStamp;
+
+        imuTimeStamp = msg->header.stamp;
+
+        // RCLCPP_INFO(this->get_logger(), "imu: %ld ", imuTimeStamp.nanoseconds());
+        RCLCPP_INFO(this->get_logger(), "imu elapsed time: %ld ", elapse.nanoseconds());
+
+
+    }
+
+
+    void pointcloudCallback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr &msg)
+    {
+        rclcpp::Time current = this->now();
+        pointcloudTimeStamp = msg->header.stamp;
+
+        // Calculate the duration
+        rclcpp::Duration duration = pointcloudTimeStamp - imuTimeStamp;
+        // rclcpp::Duration duration = pointcloudTimeStamp - current;
+
+        // Log the duration
+        // RCLCPP_INFO(this->get_logger(), "Now     : %ld", current.nanoseconds());
+        // RCLCPP_INFO(this->get_logger(), "pcl rcv : %ld ", pointcloudTimeStamp.nanoseconds());
+        // RCLCPP_INFO(this->get_logger(), "imu rcv : %ld ", imuTimeStamp.nanoseconds());
+        RCLCPP_INFO(this->get_logger(), "Duration: %ld", duration.nanoseconds());
     }
 };
+
 
 int main(int argc, char * argv[])
 {
